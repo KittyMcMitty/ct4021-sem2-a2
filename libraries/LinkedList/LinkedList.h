558651c4e9d3a90b06356db9fb3de0f1ef935a8c
Nick Ives <s1909632@connect.glos.ac.uk>
2020-04-12 05:29:47 +0100

    LinkedList reformatting and tidy up

--- libraries/LinkedList/LinkedList.h
+++ libraries/LinkedList/LinkedList.h
@@ -18,7 +18,7 @@ template<typename T>
 class ListNode {
   friend class LinkedList<T>;
  private:
-  T data_ {0};
+  T data_;
   ListNode<T>* next_ {nullptr};
 
  public:
@@ -40,104 +40,119 @@ template<typename T>
 class LinkedList {
 
  private:
-  ListNode<T>* head_ {nullptr};
-  ListNode<T>* iterate_ptr_ {head_}; //used by iterate()
+  ListNode<T> *head_{nullptr};
+  ListNode<T> *tail_{nullptr};
+  ListNode<T> *iterate_ptr_{head_}; //used by iterator methods / operators
 
  public:
+  ~LinkedList();
+  void insert(T data);
+  void remove(T data);
+  T iterate();
 
-  // Destructor. Deletes all list nodes.
-  ~LinkedList() {
-    ListNode<T>* current_ptr = head_;
-    ListNode<T>* old_ptr = nullptr;
-
-    // while current pointer points to something...
-    while (current_ptr != nullptr) {
-      old_ptr = current_ptr; // save that pointer
-      current_ptr = current_ptr->next_; // advance to next pointer
-      delete old_ptr; // and now delete that object
-    }
-  }
-
-  /*
-   * Insert - add an item to the list
-   *
-   * This will stick an item at the START of the list. This is makes this nice
-   * and fast. So, items should be added in REVERSE order as the things you add
-   * later will be return before, if that is important to you.
-   */
-  void insert(T data) {
-
-    // if list is not empty (more common case)
-    if (head_ != nullptr) {
-      auto node = new ListNode<T>(data, head_);
-      head_ = node;
-
-    } else {
-      auto node = new ListNode<T>(data, nullptr);
-      head_ = node;
-      //iterate_ptr_ = node; // stop iterate_ptr_ from being null
-    }
-  };
-
-  /*
-   * Remove - remove an entry from the list.
-   *
-   * This method will iterate over the list and removew the first matching
-   * entry found. If the same thing has been added twice, it will only be
-   * deleted once!
-   */
-  void remove(T data) {
-
-    ListNode<T>* current_ptr = head_;
-    ListNode<T>* old_ptr = nullptr;
-
-    // while we're not at the end
-    while (current_ptr !=nullptr) {
-      // if we find a match..
-      if (current_ptr->data_ == data) {
-        if (old_ptr == nullptr) { // this is true if we're still at head
-          head_ = current_ptr->next_; // make head the next one
-          delete current_ptr;
-          break; // break out of loop here
-
-        } else {
-          old_ptr->next_ = current_ptr->next_;
-          delete current_ptr;
-          break;
-        }
-      } else {
-        old_ptr = current_ptr;
-        current_ptr = current_ptr->next_;
-      }
-
-    }
-  };
-
-  /*
-   * Iterate - iterates over the list
-   *
-   * This will return the data from the current list node and then advance
-   * iterate_ptr to the next node in the list. If iterate_ptr is nullptr, reset
-   * iterate_ptr back to head_ and return null so the caller knows the end
-   * is nigh.
-   *
-   * Of note: items added to the list while it is iterating won't be returned
-   * until the next iteration round, as they are inserted at the head.
-   */
-  T iterate() {
-    // if we aren't at the end of the list
-    if (iterate_ptr_ != nullptr) {
-      ListNode<T>* old_ptr = iterate_ptr_;  // save current pointer
-      iterate_ptr_ = iterate_ptr_->next_;   // advance iterator
-      return old_ptr->data_;                // return saved data
-
-    } else {
-      iterate_ptr_ = head_; // reset iterator back to head
-      return NULL;       // return null
-    }
-  };
 };
 
+/*
+ * ~LinkedList - Destructor
+ *
+ * This destructor traverses the list and delete's every node.
+ */
+template<typename T>
+LinkedList<T>::~LinkedList() {
 
+  ListNode<T> *current_ptr = head_;
+  ListNode<T> *old_ptr = nullptr;
+
+  // while current pointer points to something...
+  while (current_ptr!=nullptr) {
+    old_ptr = current_ptr; // save that pointer
+    current_ptr = current_ptr->next_; // advance to next pointer
+    delete old_ptr; // and now delete that object
+  }
+}
+
+/*
+ * Insert - add an item to the list
+ *
+ * This will stick an item at the START of the list. This is makes this nice
+ * and fast. So, items should be added in REVERSE order as the things you add
+ * later will be return before, if that is important to you.
+ */
+template<typename T>
+void LinkedList<T>::insert(T data) {
+
+  // if list is not empty (more common case)
+  if (head_ != nullptr) {
+    auto node = new ListNode<T>(data, head_);
+    head_ = node;
+
+  } else {
+    auto node = new ListNode<T>(data, nullptr);
+    head_ = node;
+    //iterate_ptr_ = node; // stop iterate_ptr_ from being null
+  }
+}
+
+
+/*
+ * Remove - remove an entry from the list.
+ *
+ * This method will iterate over the list and removew the first matching
+ * entry found. If the same thing has been added twice, it will only be
+ * deleted once!
+ */
+template<typename T>
+void LinkedList<T>::remove(T data) {
+
+  ListNode<T>* current_ptr = head_;
+  ListNode<T>* old_ptr = nullptr;
+
+  // while we're not at the end
+  while (current_ptr !=nullptr) {
+    // if we find a match..
+    if (current_ptr->data_ == data) {
+      if (old_ptr == nullptr) { // this is true if we're still at head
+        head_ = current_ptr->next_; // make head the next one
+        delete current_ptr;
+        break; // break out of loop here
+
+      } else {
+        old_ptr->next_ = current_ptr->next_;
+        delete current_ptr;
+        break;
+      }
+    } else {
+      old_ptr = current_ptr;
+      current_ptr = current_ptr->next_;
+    }
+  }
+}
+
+
+/*
+ * Iterate - iterates over the list
+ *
+ * This will return the data from the current list node and then advance
+ * iterate_ptr to the next node in the list. If iterate_ptr is nullptr, reset
+ * iterate_ptr back to head_ and return null so the caller knows the end
+ * is nigh.
+ *
+ * Of note: items added to the list while it is iterating won't be returned
+ * until the next iteration round, as they are inserted at the head.
+ */
+template<typename T>
+T LinkedList<T>::iterate() {
+  // if we aren't at the end of the list
+  if (iterate_ptr_ != nullptr) {
+    ListNode<T>* old_ptr = iterate_ptr_;  // save current pointer
+    iterate_ptr_ = iterate_ptr_->next_;   // advance iterator
+    return old_ptr->data_;                // return saved data
+
+  } else {
+    iterate_ptr_ = head_; // reset iterator back to head
+    T empty;
+    return empty; // return an empty object
+  }
+}
 
 #endif //A_TOOLCHAIN_TEST_LIBRARIES_LINKEDLIST_LINKEDLIST_H_
